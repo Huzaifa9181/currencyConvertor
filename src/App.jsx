470312179f3117fd0ba84c132275b8c514bd6ba8
huzaifa-sidtechno <141606950+huzaifa-sidtechno@users.noreply.github.com>
2024-01-15 18:20:20 +0500

    add functionality

--- src/App.jsx
+++ src/App.jsx
@@ -7,15 +7,12 @@ import InputBox from './Components/InputBox';
 
 function App() {
   const [count, setCount] = useState(0)
-  const [label , setLabel] = useState('From')
-  const [labelAnother , setLabelAnother] = useState('To')
-  const [to , setTo] = useState("usd")
 
   return (
     <>
       <div className="container-fluid bg-secondary w-100 d-flex align-items-center justify-content-center" style={{ height: '100vh' }}>
         <div className="Box-container p-4" style={{ width: '45%' }}>
-          <InputBox label={label}  labelAnother={labelAnother}  />
+          <InputBox/>
           
         </div>
       </div>
